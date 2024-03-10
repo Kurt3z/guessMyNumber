e218cedae3c60728209c23e6544e45aad93c8525
Tiago Pinto <tiago.webdev10@gmail.com>
2024-03-10 17:00:14 +0000

    Complete JavaScript functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const usernameSubmitBtn = document.querySelector(".submit");
+const resetBtn = document.querySelector(".reset");
 const usernameInputEl = document.querySelector(".username-input");
 const playerLabelEl = document.querySelector(".player");
 const secretNumberEl = document.querySelector(".number");
@@ -13,14 +14,33 @@ const highscoreEl = document.querySelector(".highscore");
 
 const messagesBox = document.querySelector(".messages");
 
+const triesBox = document.querySelector(".tries");
+
 const generateSecretNumber = function () {
   return Math.trunc(Math.random() * 20) + 1;
+};
+
+const createHearts = function (num) {
+  for (let i = 0; i < num; i++) {
+    const hearthSpan = document.createElement("span");
+    hearthSpan.textContent = "❤️";
+    hearthSpan.classList.add(`try-${i}`);
+    triesBox.appendChild(hearthSpan);
+  }
+};
+
+const appendMessage = function (message) {
+  messagesBox.insertBefore(message, messagesBox.firstChild);
 };
 
 let secretNumber = generateSecretNumber();
 let score = 20;
 let highscore = 0;
 console.log(secretNumber);
+
+window.addEventListener("load", function () {
+  createHearts(20);
+});
 
 usernameSubmitBtn.addEventListener("click", function () {
   if (username) {
@@ -33,33 +53,76 @@ usernameSubmitBtn.addEventListener("click", function () {
 guessBtn.addEventListener("click", function () {
   const guess = Number(guessInputEl.value);
   const message = document.createElement("p");
-  console.log(guess, secretNumber);
-
-  if (!guess) {
-    message.classList.add("message");
-    message.textContent = "⛔ Please insert a number";
-    messagesBox.appendChild(message);
-  }
-
-  if (guess === secretNumber) {
-    message.textContent = "🏆 You won";
-    messagesBox.appendChild(message);
-    if (score > highscore) {
-      highscore = score;
-    }
-    highscoreEl.textContent = score;
-    guessBtn.disabled = true;
-    secretNumberEl.textContent = secretNumber;
+  const username = playerLabelEl.textContent;
+  console.log(username);
+  if (!username) {
+    alert("Please insert a username to start playing!");
   } else {
-    if (score > 1) {
-      message.textContent = guess > secretNumber ? "📈 Too high" : "📉 Too low";
-      messagesBox.appendChild(message);
-      score--;
-      scoreEl.textContent = score;
-    } else {
+    if (!guess) {
+      message.classList.add("message");
+      message.textContent = "⛔ Please insert a number";
+      appendMessage(message);
+    } else if (guess === secretNumber) {
+      message.textContent = "🏆 You won";
+      appendMessage(message);
+      if (score > highscore) {
+        highscore = score;
+      }
+      highscoreEl.textContent = score;
       guessBtn.disabled = true;
-      message.textContent = "🧨 You lost. Reset the game to try again!";
-      messagesBox.appendChild(message);
+      secretNumberEl.textContent = secretNumber;
+    } else {
+      if (score > 1) {
+        message.textContent = guess > secretNumber ? "📈 Too high" : "📉 Too low";
+        appendMessage(message);
+        score--;
+        scoreEl.textContent = score;
+
+        const hearth = document.querySelector(`.try-${score}`);
+        hearth.remove();
+      } else {
+        guessBtn.disabled = true;
+        score = 0;
+        scoreEl.textContent = score;
+        const hearth = document.querySelector(`.try-${score}`);
+        hearth.remove();
+        message.textContent = "🧨 You lost. Reset the game to try again!";
+        appendMessage(message);
+      }
     }
   }
+});
+
+resetBtn.addEventListener("click", function () {
+  // Generate new secret number
+  secretNumber = generateSecretNumber();
+
+  // Hide secret number again
+  secretNumberEl.textContent = "?";
+
+  // Reset hearths
+  for (let i = score; i <= 20; i++) {
+    const hearthSpan = document.createElement("span");
+    hearthSpan.textContent = "❤️";
+    hearthSpan.classList.add(`try-${i}`);
+    triesBox.appendChild(hearthSpan);
+  }
+
+  // Reset score
+  score = 20;
+  scoreEl.textContent = 20;
+
+  // Reset messages box
+  while (messagesBox.lastElementChild) {
+    messagesBox.removeChild(messagesBox.lastElementChild);
+  }
+  const initialParagraph = document.createElement("p");
+  initialParagraph.innerHTML = "<span class='prompt'> &raquo;</span>Start by trying to guess the number";
+  messagesBox.appendChild(initialParagraph);
+
+  // Make guess button available again
+  guessBtn.disabled = false;
+
+  // Clear input box
+  guessInputEl.value = "";
 });
